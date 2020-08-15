5a616c550ecd88a18aa5846aa5ed24f6dd9ced80
Will Lytle <wlytle89@gmail.com>
2020-08-15 17:33:04 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -8,6 +8,6 @@ end
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
   array.length do |element|
-    element.is_a? Strng && element.lenght == 0
+    element.is_a? Strng && element.length == 0
   end
 end
