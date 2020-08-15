bc93638fb5281ff6bb514586d390445a42e1668e
Will Lytle <wlytle89@gmail.com>
2020-08-15 17:56:36 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
   array.count do |element|
@@ -9,7 +9,6 @@ end
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
   array.count do |element|
-    binding.pry
     element.class == String && element.to_s.empty?
   end
 end
